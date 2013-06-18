daf3f85fc2e5160db8e91a84c8766a248e260d79
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-18 17:47:29 +0530

    Changed jdk manifests

--- modules/jdk/manifests/init.pp
+++ modules/jdk/manifests/init.pp
@@ -1,14 +1,16 @@
-exec { 'download oracle-jdk':
-	cwd		=> '/opt/',
-	command	=> '/usr/bin/wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"  http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin --no-check-certificate',
-}
+class jdk {
+	exec { 'download oracle-jdk':
+		cwd		=> '/opt/',
+		command	=> '/usr/bin/wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"  http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin --no-check-certificate',
+	}
 
-exec { 'change_permission_jdk':
-	command	=> 'mv jdk-6u45-linux-x64.bin?A* jdk-6u45-linux-x64.bin && chmod a+x jdk-6u45-linux-x64.bin',
-	require		=> Exec['download oracle-jdk'],
-}
+	exec { 'change_permission_jdk':
+		command	=> 'mv jdk-6u45-linux-x64.bin?A* jdk-6u45-linux-x64.bin && chmod a+x jdk-6u45-linux-x64.bin',
+		require		=> Exec['download oracle-jdk'],
+	}
 
-exec {'extract_jdk':
-	command	=> './jdk-6u45-linux-x64.bin',
-	require		=> Exec['change_permission_jdk'],
+	exec {'extract_jdk':
+		command	=> './jdk-6u45-linux-x64.bin',
+		require		=> Exec['change_permission_jdk'],
+	}
 }
