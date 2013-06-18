0a716a8d00c511a4d26adf7bdf61d0bd9694d4be
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-18 17:56:20 +0530

    Changed jdk manifests

--- modules/jdk/manifests/init.pp
+++ modules/jdk/manifests/init.pp
@@ -1,4 +1,7 @@
 class jdk {
+	package {'wget':
+		ensure	=> installed,
+	}
 	exec { 'download oracle-jdk':
 		cwd		=> '/opt/',
 		command	=> '/usr/bin/wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"  http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin --no-check-certificate',
