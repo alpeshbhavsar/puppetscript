4a4edb005198e665ff5008939585e439454acf32
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-18 17:27:57 +0530

    Removed Key from Pclient1

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -17,6 +17,6 @@ node 'pclient1' {
 	ssh_authorized_key { 'root_pserver':
 		user	=> 'root',
 		type	=> 'rsa',
-		key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAwk4htthZQfuI5ZSU2EUatWWEmEnhLxubB322aY37WzqRnrnpx5nZexh6zIgrjBzpgJTbuhwVaGGI1KbV0+E06l20DApisJ1TAQT+9rwKRMZ2uiA7ma7MHTmY2asySZBZHPDxNXWFH0H9Mvu25EEJi37H/Ah2yBSXSOVPuaoAo+Tr8pOd5ZBOFmpALHujWs4WSXNHwRUIRaATShlkvj7VTatJhT8uwyqqKsOz/6OfT2gDozRBw66QhFtudEUbF1waeVcg6h6s/VgcqYLKmcPVTIBvgy0oW//ubr51j0oFWDJhRq/igYg2/xzcloZRQwx0vvxfzFd3VXkjQNbP7Hx5ww== ',
+		key	=> '',
 	}
 }
