acdd6f233b98b572732f40d25703277079726ad5
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-18 17:24:22 +0530

    Added root Public Key of Pserver to pclient1's knownhosts

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -14,4 +14,9 @@ node 'pclient1' {
 		home	=> '/home/jishnu',
 		managehome	=> true,
 	}
+	ssh_authorized_key { 'root_pserver':
+		user	=> 'root',
+		type	=> 'rsa',
+		key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAwk4htthZQfuI5ZSU2EUatWWEmEnhLxubB322aY37WzqRnrnpx5nZexh6zIgrjBzpgJTbuhwVaGGI1KbV0+E06l20DApisJ1TAQT+9rwKRMZ2uiA7ma7MHTmY2asySZBZHPDxNXWFH0H9Mvu25EEJi37H/Ah2yBSXSOVPuaoAo+Tr8pOd5ZBOFmpALHujWs4WSXNHwRUIRaATShlkvj7VTatJhT8uwyqqKsOz/6OfT2gDozRBw66QhFtudEUbF1waeVcg6h6s/VgcqYLKmcPVTIBvgy0oW//ubr51j0oFWDJhRq/igYg2/xzcloZRQwx0vvxfzFd3VXkjQNbP7Hx5ww== ',
+	}
 }
