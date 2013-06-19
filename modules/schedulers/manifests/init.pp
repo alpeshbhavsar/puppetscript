76e92525db157080c111668f9ce360604be96cb7
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-19 12:19:00 +0530

    Added Schedulers

--- modules/schedulers/manifests/init.pp
+++ modules/schedulers/manifests/init.pp
@@ -1,8 +1,8 @@
 class schedulers {
 	cron { 'time sync':
 		command	=> '/usr/sbin/ntpdate -u time.windows.com',
-		minute	=> '*/1',
-		hour	=> '*',
+		minute	=> '0',
+		hour	=> '*/2',
 		month	=> '*',
 		weekday	=> '*',
 	}
