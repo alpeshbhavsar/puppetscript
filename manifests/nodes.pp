ef11b9cec6e4c5e5d1f16145af81e344fbe5f5fe
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-19 15:12:35 +0530

    Added Vhost for pclient1/2 through template

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -8,20 +8,18 @@ node 'pclient1' {
 	include vnc
 	include subversion
 	include git
-}
-
-node 'pclient1', 'pclient2' {
+	
 	include nginx
 
-	$site_name = 'abc-com'
-	$site_domain = 'abc.com'
-	file { '/etc/nginx/conf.d/abc.com.conf':
-		ensure	=> present,
-		content	=> template('nginx/vhost.conf.erb'),
-		notify	=> Service['nginx'],
-	}
+        $site_name = 'abc-com'
+        $site_domain = 'abc.com'
+        file { '/etc/nginx/conf.d/abc.com.conf':
+                ensure  => present,
+                content => template('nginx/vhost.conf.erb'),
+                notify  => Service['nginx'],
+        }
 
-	$site_name = 'xyz-com'
+        $site_name = 'xyz-com'
         $site_domain = 'xyz.com'
         file { '/etc/nginx/conf.d/xyz.com.conf':
                 ensure  => present,
@@ -31,6 +29,7 @@ node 'pclient1', 'pclient2' {
 
 
 }
+
 
 
 node default {
