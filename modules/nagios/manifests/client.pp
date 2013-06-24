460126a2bf07d5daa9fcf6777003d190c839c36e
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-24 15:13:58 +0530

    Added Nagios Client

--- modules/nagios/manifests/client.pp
+++ modules/nagios/manifests/client.pp
@@ -2,5 +2,20 @@ define nagios::client( $nagiosserverIP ) {
 	package	{ 'nagios-nrpe':
 		ensure	=> installed,
 	}
+	file {'/etc/nagios/nrpe.conf':
+		ensure	=> present,
+	}
 	
+	exec {'checknrpefileexists':
+		command	=> 'cat /etc/nagios/nrpe.conf',
+	}
+	exec {'allownagiosserver':
+		command	=> 'sed -i".bak" '/allowed_hosts/d' /etc/nagios/nrpe.conf',
+		command => 'echo "allowed_hosts=$nagiosserverIP" >> /etc/nagios/nrpe.conf',
+		require	=> Exec['checknrpefileexists'],
+	}
+	exec {'restartnrpeclient':
+		command	=> '/etc/init.d/nrpe restart',
+		require	=> Exec['allownagiosserver'],
+	}
 }
