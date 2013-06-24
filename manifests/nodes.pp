06380a52cceb7b319883ec869ab33a9babec2a8e
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-24 17:08:05 +0530

    Added Nagios Client

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,7 +7,9 @@ node 'pserver' {
 
 node 'nagiosserver' {
 	include httpd, nagios
-	nagios::server::hostAdd {'pclient1','192.168.131.137'}
+	nagios::server::hostAdd {'test':
+	$clienthostname = 'pclient1'
+	$clientIPaddress = '192.168.131.137'
 	}
 
 node 'pclient1' {
