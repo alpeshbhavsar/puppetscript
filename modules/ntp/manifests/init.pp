8aab60aa711ee056cc0bb7c20a11459ad1d11ab1
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-25 16:37:39 +0530

    Added Nagios Client

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -13,4 +13,9 @@ class ntp($server='UNSET') {
 		enable	=> true,
 		require	=> [ Package['ntp'], File['/etc/ntp.conf']],
 	}
+	
+	exec { "synctime":
+		command	=> 'ntpdate -u time.windows.com',
+		path	=> ['/bin', '/usr/bin', '/usr/sbin'],
+	}
 }
