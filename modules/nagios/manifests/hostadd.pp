9e28370131ead9c8707b0173a73580305ce93b2a
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-25 15:52:59 +0530

    Added Nagios Client

--- modules/nagios/manifests/hostadd.pp
+++ modules/nagios/manifests/hostadd.pp
@@ -1,6 +1,6 @@
 define nagios::hostadd($clienthostname , $clientIPaddress ){
 	include nagios 
-	file {'/etc/nagios/objects/$clienthostname.cfg':
+	file {"/etc/nagios/objects/$clienthostname.cfg":
 		ensure	=> present,
 		content	=> template('nagios/client.conf.erb'),
 	}
