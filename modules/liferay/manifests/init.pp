d1043f10ffe2276009c9b4e7b06688461213fc30
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-06-27 12:43:42 +0530

    Added Liferay Manifests

--- modules/liferay/manifests/init.pp
+++ modules/liferay/manifests/init.pp
@@ -18,7 +18,7 @@ class liferay {
       exec {'extractliferay':
           cmd => '/opt',
           command => 'unzip /opt/liferay-portal-tomcat-6.1.1-ce-ga2-20120731132656558.zip && mv /opt/liferay-porta* /opt/liferay',
-          path  => '['/usr', '/usr/bin', '/usr/sbin']',
+          path  => ['/usr', '/usr/bin', '/usr/sbin'],
           require => Exec['downloadliferay'],
       }
       
