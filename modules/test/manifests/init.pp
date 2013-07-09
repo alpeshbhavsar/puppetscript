2823d638294a20c256e0496255b0e10278d61019
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-07-09 17:54:00 +0530

    test

--- modules/test/manifests/init.pp
+++ modules/test/manifests/init.pp
@@ -1,13 +1,9 @@
-class test12{
+class test12 {
 	file {"/root/hostgroup.txt":
 		ensure	=> present,
 	}
-
 	$lists = "`gawk -Fmembers '{ print $2 }' /root/hostgroup.txt`"
-	
 	exec {'addcontent':
 		command	=> "echo $lists > /tmp/free.txt",
 	}
-		
-
 }
