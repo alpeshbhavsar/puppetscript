956d770d6963bae3f53c6655d091f306b8361584
Alpesh Bhavsar <alpesh.bhavsar@cignex.com>
2013-07-09 17:59:43 +0530

    test

---
+++ modules/testingz/manifests/init.pp
@@ -0,0 +1,9 @@
+class testingz {
+	file {"/root/hostgroup.txt":
+		ensure	=> present,
+	}
+	$lists = "`gawk -Fmembers '{ print $2 }' /root/hostgroup.txt`"
+	exec {'addcontent':
+		command	=> "echo $lists > /tmp/free.txt",
+	}
+}
